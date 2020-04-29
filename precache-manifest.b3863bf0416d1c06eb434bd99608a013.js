481f811ab79f67af5d4e956a0c414f250c3ad5cf
kamati hasheela <kamatihasheela@gmail.com>
2020-04-29 11:25:05 +0200

    Updates

---
+++ precache-manifest.b3863bf0416d1c06eb434bd99608a013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b232c19716dff75185236a998e46e7e",
+    "url": "/Star-Grid-Public/index.html"
+  },
+  {
+    "revision": "2e04c7173d18f4ec496d",
+    "url": "/Star-Grid-Public/static/css/2.20155695.chunk.css"
+  },
+  {
+    "revision": "120eb0a3ff66fb5575d2",
+    "url": "/Star-Grid-Public/static/css/main.e1521781.chunk.css"
+  },
+  {
+    "revision": "2e04c7173d18f4ec496d",
+    "url": "/Star-Grid-Public/static/js/2.fac4536a.chunk.js"
+  },
+  {
+    "revision": "f191614ddef141d17fbd85c38a503f4b",
+    "url": "/Star-Grid-Public/static/js/2.fac4536a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120eb0a3ff66fb5575d2",
+    "url": "/Star-Grid-Public/static/js/main.60484d49.chunk.js"
+  },
+  {
+    "revision": "277ebe76ed0e80a743e4",
+    "url": "/Star-Grid-Public/static/js/runtime-main.26d7b669.js"
+  },
+  {
+    "revision": "b21416c8ccba81f4d8b1642ab1115e80",
+    "url": "/Star-Grid-Public/static/media/Capture_burned.b21416c8.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Star-Grid-Public/precache-manifest.b3863bf0416d1c06eb434bd99608a013.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Star-Grid-Public/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
