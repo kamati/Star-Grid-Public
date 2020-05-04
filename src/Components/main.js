7edbaa4a76d8c1552241c4254274ef7c816747f9
kamati hasheela <kamatihasheela@gmail.com>
2020-05-04 23:06:42 +0200

    meterhome

--- src/Components/main.js
+++ src/Components/main.js
@@ -6,11 +6,11 @@ import alarms from './alarms';
 import GridTopology from './gridTopology';
 import MeterProps from './MeterOverView/MeterProps';
 import Home from './home';
-import FormMeter from './FormMeter'
+import FormMeter from './FormMeter';
 
 const Main = () => (
 	<Switch>
-		<Route exact path='/Home' component={Home} />
+		<Route exact path='/' component={MapDash} />
 		<Route exact path='/gridTopology' component={GridTopology} />
 		<Route exact path='/systemSetting' component={systemSetting} />
 		<Route exact path='/alarms' component={alarms} />
