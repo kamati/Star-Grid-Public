23052ce1a03f171abbad94105d8e0d8bcfdaf370
kamati hasheela <kamatihasheela@gmail.com>
2020-06-25 22:05:47 +0200

    system Setting3

--- src/Components/SystemSettings/Routing.js
+++ src/Components/SystemSettings/Routing.js
@@ -8,11 +8,10 @@ import ProfileInfo from './ProfileInfo';
 
 const Routing = () => (
 	<Switch>
-		<Route exact path='/SystemSetting' component={Trarrif} />
-		<Route exact path='/SystemSetting/Trarrif' component={Trarrif} />
-		<Route exact path='/SystemSetting/UserManagment' component={UserManagment} />
-		<Route exact path='/SystemSetting/Password' component={Password} />
-		<Route exact path='/SystemSetting/ProfileInfo' component={ProfileInfo} />
+		<Route path='/SystemSetting/Trarrif' component={Trarrif} />
+		<Route path='/SystemSetting/UserManagment' component={UserManagment} />
+		<Route path='/SystemSetting/Password' component={Password} />
+		<Route path='/SystemSetting/ProfileInfo' component={ProfileInfo} />
 	</Switch>
 );
 export default Routing;
